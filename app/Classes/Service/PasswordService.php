b3546b4b96e68794fae5b57b227d30c2da89b7ef
henrikgebauer <code@henrik-gebauer.de>
2025-08-22 23:11:28 +0200

    remove legacy password check

--- app/Classes/Service/PasswordService.php
+++ app/Classes/Service/PasswordService.php
@@ -15,50 +15,6 @@ class PasswordService
     const CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890';
     const ITERATIONS = 100000;
 
-    /**
-     * Prüft, ob $hash zu $plaintext passt
-     *
-     * @param string $hash
-     * @param string $plaintext
-     * @param int|null $uid User-Id
-     * @throws \UnexepectedValueException wenn $uid===null ist, aber zum Hashen benötigt wird
-     * @throws \UnexepectedValueException wenn $hash einen unbekannten Hash-Type enthält
-     */
-    public static function check(string $hash, string $plaintext, $uid = null) : bool
-    {
-        if (!$hash) {
-            return false;
-        }
-
-        if ($hash[0] === ':') {
-            $elements = explode(':', $hash);
-            $type = $elements[1];
-        } else {
-            if ($uid === null) {
-                throw new \UnexpectedValueException('$uid===null, aber alter Hash mit gehashter User-ID getestet', 1493681735);
-            }
-            return $hash === md5("$uid-" . md5($plaintext));
-        }
-
-        switch ($type) {
-        case 'A':
-            return md5($plaintext) === $elements[2];
-        case 'B':
-            $salt = $elements[2];
-            $realhash = $elements[3];
-            return md5("$salt-" . md5($plaintext)) === $realhash;
-        case 'pbkdf2':
-            $algo = $elements[2];
-            $iterations = (int)$elements[3];
-            $length = (int)$elements[4];
-            $salt = base64_decode($elements[5], true);
-            $realhash = $elements[6];
-            return base64_encode(hash_pbkdf2($algo, $plaintext, $salt, $iterations, $length, true)) === $realhash;
-        default:
-            throw new \UnexpectedValueException('Der Passwort-Typ in der Datenbank ist unbekannt', 1493681740);
-        }
-    }
-
     public static function hash(string $password) : string
     {
         // $5$ = SHA256 (LDAP compatible)
